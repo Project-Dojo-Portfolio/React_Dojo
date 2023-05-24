a1f57d3827985e88de8cacd8d7d3f1977450c29f
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-24 12:55:09 -0500

    RT-77 added event onclick in add category

--- Gif-expert-app/src/GifExpertApp.jsx
+++ Gif-expert-app/src/GifExpertApp.jsx
@@ -3,10 +3,15 @@ import { useState } from "react"
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['one Punch']);
 
+    const onAddCategory = () => {
+        setCategories([...categories,'Dragon ball z'])
+    }
+
     return (
         <>  
             {/* titulo */}
             <h1>GifExpertApp</h1>
+            <button onClick={onAddCategory}>Agregar</button>
             <ol>
                 {categories.map(category => <li key={category}>{category}</li>)}
             </ol>
