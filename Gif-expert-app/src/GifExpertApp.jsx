cff5ec1305ed7060a3c2bdc122a6f593d34f1e17
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-24 12:46:27 -0500

    RT-76 category list

--- Gif-expert-app/src/GifExpertApp.jsx
+++ Gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['one Punch']);
+
     return (
         <>  
             {/* titulo */}
             <h1>GifExpertApp</h1>
+            <ol>
+                {categories.map(category => <li key={category}>{category}</li>)}
+            </ol>
         </>
     )
 }
