ae90090b0bb5d43d33449565d4d574a77d73d71a
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-25 11:30:02 -0500

    RT-79 AddCategory and communication between components

--- Gif-expert-app/src/GifExpertApp.jsx
+++ Gif-expert-app/src/GifExpertApp.jsx
@@ -12,7 +12,7 @@ export const GifExpertApp = () => {
     <>
       {/* titulo */}
       <h1>GifExpertApp</h1>
-      <AddCategory />
+      <AddCategory setCategories={setCategories}/>
       <button onClick={onAddCategory}>Agregar</button>
       <ol>
         {categories.map((category) => (
--- Gif-expert-app/src/components/AddCategory.jsx
+++ Gif-expert-app/src/components/AddCategory.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export const AddCategory = () => {
+export const AddCategory = ({setCategories}) => {
   const [inputValue, setInputValue] = useState("");
   const onInputChange = (event) => {
     setInputValue(event.target.value);
@@ -8,9 +8,12 @@ export const AddCategory = () => {
 
   const onSubmit = (event) => {
     event.preventDefault();
+    if(inputValue.trim().length <= 1) return;
+    setCategories(categories => [...categories, inputValue]);
+    setInputValue('');
   };
   return (
-    <form onSubmit={}>
+    <form onSubmit={onSubmit}>
         <input
             type="text"
             placeholder="Buscar Gif"
