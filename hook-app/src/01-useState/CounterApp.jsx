27554207f5bbcea526d600c2d2703cdec0ffab85
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-06-12 17:34:16 -0500

    exponer metodos custom hooks

---
+++ hook-app/src/01-useState/CounterApp.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function CounterApp() {
+  const [{ counter1, counter2, counter3 }, setCounter] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+  return (
+    <>
+      <h1>Counter:{counter1} </h1>
+      <h1>Counter:{counter2} </h1>
+      <h1>Counter:{counter3} </h1>
+      <hr />
+      <button
+        className="btn btn-primary"
+        onClick={() =>
+          setCounter({ counter1: counter1 + 1, counter2, counter3 })
+        }
+      >
+        +1
+      </button>
+    </>
+  );
+}
