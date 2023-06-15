1b3d506d67f0d28fc6e9e68faf4649e5253231d4
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-06-15 11:12:42 -0500

    useCallback con argumentos

--- hook-app/src/06-memos/CallbackHook.jsx
+++ hook-app/src/06-memos/CallbackHook.jsx
@@ -3,8 +3,8 @@ import { ShowIncrement } from "./ShowIncrement";
 
 export const CallbackHook = () => {
   const [counter, setCounter] = useState(10);
-  const increment = useCallback(() => {
-    setCounter((value) => value + 1);
+  const increment = useCallback((value) => {
+    setCounter((c) => c + value);
   }, []);
 
   return (
