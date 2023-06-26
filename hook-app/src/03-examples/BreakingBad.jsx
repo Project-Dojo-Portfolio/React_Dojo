4723949119b56403c982c5f0d617b1462c908624
Kevin Andres Ortiz Merchan <kevinandresortizmerchan@outlook.com>
2023-06-26 12:23:27 -0500

    add test useFetch and useCount
    Evaluar request del useFetch

--- hook-app/src/03-examples/BreakingBad.jsx
+++ hook-app/src/03-examples/BreakingBad.jsx
@@ -1,30 +1,15 @@
 import Proptypes from 'prop-types';
-import { useState } from 'react';
-import { useLayoutEffect, useRef } from 'react';
 
 export const BreakingBad = ({author, quote}) => {
-    const pRef = useRef();
-    const [boxSize, setBoxSize] = useState({
-        width: 0,
-        height:0
-    })
-    useLayoutEffect(() => {
-        const {height, width} = pRef.current.getBoundingClientReact();
-        setBoxSize({
-            height,
-            width
-        })
-    }, [quote])
 
   return (
     <>
     <blockquote
             className='blockquote text-end'
             style={{display: 'flex'}}>
-        <p ref={pRef}>{quote}</p>
+        <p>{quote}</p>
         <footer className="blockquote-footer">{author}</footer>
     </blockquote>
-    <code>{JSON.stringify(boxSize)}</code>
     </>
   )
 }
---
+++ hook-app/tests/03-examples/MultipleCustomHooks.test.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable no-undef */
+import { fireEvent, render, screen } from "@testing-library/react"
+import { MultipleCustomHooks } from "../../src/03-examples/MultipleCustomHooks";
+import { useCounter, useFetch } from "../../src/hooks";
+jest.mock('../../src/hooks/useFetch');
+jest.mock('../../src/hooks/useCounter');
+
+
+describe('Pruebas en <MultipleCustomHooks />', () => { 
+    const mockIncrement = jest.fn();
+    useCounter.mockReturnValue({
+        counter: 1,
+        increment: mockIncrement
+    })
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+    test('debe de mostrar el componente por defecto', () => {
+        useFetch.mockReturnValue({
+            data:null,
+            isLoading: true,
+            hasError: null
+        })
+        render(<MultipleCustomHooks />);
+        expect(screen.getByText('Cargando.....'));
+        expect(screen.getByText('breakingBad Quotes'));
+        const nextButton = screen.getByRole('button', {name: 'next quote'});
+        expect(nextButton.disabled).toBeFalsy();
+     })
+
+     test('Debe de mostrar un Quote ', () => {
+        useFetch.mockReturnValue({
+            data:[{ 
+                author: 'Kevin', quote: 'Hola Mundo'
+            }],
+            isLoading: false,
+            hasError: null
+        })
+        render(<MultipleCustomHooks />);
+        expect(screen.getByText('Hola Mundo')).toBeTruthy();
+        expect(screen.getByText('Kevin')).toBeTruthy();
+        const nextButton = screen.getByRole('button', {name: 'next quote'});
+        expect(nextButton.disabled).toBeFalsy();
+     });
+     test('Debe de llamar la funcion de incrementar', () => {
+        useFetch.mockReturnValue({
+            data:[{ 
+                author: 'Kevin', quote: 'Hola Mundo'
+            }],
+            isLoading: false,
+            hasError: null
+        })
+
+        render(<MultipleCustomHooks />);
+        const nextButton = screen.getByRole('button', {name: 'next quote'});
+        expect(screen.getByText('Hola Mundo')).toBeTruthy();
+        expect(screen.getByText('Kevin')).toBeTruthy();
+        fireEvent.click(nextButton);
+        expect(mockIncrement).toHaveBeenCalled();
+
+     })
+ })
